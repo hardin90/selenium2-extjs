91fbe97e59ae0ae15b2172b7b5055014d8e4a411
suongMai <suong.mai@infonam.com>
2015-06-19 11:04:11 +0700

    add methord get_element() to Grid element

--- src/main/python/selenium2extjs/webelements/Grid.py
+++ src/main/python/selenium2extjs/webelements/Grid.py
@@ -15,6 +15,12 @@ class Grid(Field):
         '''
         super(Grid, self).__init__(driver, query_type, query, top_element)
 
+    def get_element(self):
+        element_id = self.exec_script_on_extjs_cmp(
+            "return extCmp.id"
+        )
+        return self.driver.find_element_by_id(element_id)
+
     def get_grid_content_element(self):
         element_id = self.exec_script_on_extjs_cmp(
             "return extCmp.body.id"
